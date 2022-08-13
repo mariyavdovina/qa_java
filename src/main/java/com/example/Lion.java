219fc1cec0a237a2a912718c3582cbaf5ac1df22
Mariya.Vdovina <mariya.vdovina@gmail.com>
2022-08-13 14:11:48 +0300

    Felone injected to Lion

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,18 +5,22 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
+    Feline feline;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
+
         if ("Самец".equals(sex)) {
             hasMane = true;
+            this.feline = feline;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
+            this.feline = feline;
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
     }
 
-    Feline feline = new Feline();
+    //Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
