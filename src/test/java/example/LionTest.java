8d7b187402edd00584a55f683eff1e8b1b5600c7
Mariya.Vdovina <mariya.vdovina@gmail.com>
2022-08-13 14:35:27 +0300

    Feline injected to Lion CatTest, FelineTest added

---
+++ src/test/java/example/LionTest.java
@@ -0,0 +1,16 @@
+package example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+}
